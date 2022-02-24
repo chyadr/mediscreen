adfce564ce0e1d3f43dbf2f81eebc475d21c60e9
chyadr <chaimaafakir@gmail.com>
2022-02-24 23:21:50 +0100

    [OCR][Projet9] add user pwd for docker hub and readme

--- docker-scripts.sh
+++ docker-scripts.sh
@@ -1,8 +1,10 @@
+export DOCKER_USER=
+export DOCKER_PASS=
 ./build-patient.sh
 ./build-note.sh
 ./build-assessment.sh
 ./build-webapp.sh
-docker login
+docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 docker build . -t patient -f Dockerfile.patient
 docker build . -t note -f Dockerfile.note
 docker build . -t assessment -f Dockerfile.assessment
